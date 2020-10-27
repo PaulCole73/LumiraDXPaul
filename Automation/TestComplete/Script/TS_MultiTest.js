ef07ad0bd7d923d89dc9a33204f7e225b1d4f49b
pauldunstan-lumira <40229635+pauldunstan-lumira@users.noreply.github.com>
2020-10-27 12:04:17 +0100

    Update 1

---
+++ Automation/TestComplete/Script/TS_MultiTest.js
@@ -0,0 +1,26 @@
+﻿//USEUNIT Tested_Apps
+//USEUNIT TC_MultiTest
+//--------------------------------------------------------------------------------
+//Suite of tests for adverse events
+
+//Master Suites
+//--------------------------------------------------------------------------------
+function ts_multitest(send_mail)
+{
+  reset_folder();
+  
+  tc_multi_tests();
+  
+  email_and_archive(send_mail, "ts_multi_tests");
+}
+//--------------------------------------------------------------------------------
+function ts_staging_regression_multi_tests()
+{
+  reset_folder();
+
+  tc_multi_tests();
+  
+  email_and_archive(true, "ts_multi_tests_regression");
+}
+//General Suites
+//--------------------------------------------------------------------------------
