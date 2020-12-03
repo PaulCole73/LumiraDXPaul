7af685a7dfe889c00d11a626d495013e58dec914
pauldunstan-lumira <40229635+pauldunstan-lumira@users.noreply.github.com>
2020-12-03 14:55:28 +0100

    Update for complete test one

--- Automation/TestComplete/Script/TS_MultiTest.js
+++ Automation/TestComplete/Script/TS_MultiTest.js
@@ -8,8 +8,17 @@
 function ts_multitest(send_mail)
 {
   reset_folder();
-  
   tc_multi_tests();
+  
+  email_and_archive(send_mail, "ts_multi_tests");
+}
+//--------------------------------------------------------------------------------
+function ts_multitest_complete(send_mail,new_config_file_name,locale)
+{
+  reset_folder();
+  setup_automation(new_config_file_name,locale);
+  tc_multi_tests();
+  retest_failed_automation_tests();
   
   email_and_archive(send_mail, "ts_multi_tests");
 }
