7c18f03b5beb121f330a692e856868b419daa083
Dean Lester <dean.lester@lumiradx.com>
2021-03-24 16:19:15 +0000

    Added new clinical headings, started adding results section

--- Automation/TestComplete/Script/TC_Clinical_Controls.js
+++ Automation/TestComplete/Script/TC_Clinical_Controls.js
@@ -1,6 +1,11 @@
 ﻿//USEUNIT TC_Treatment
 
-function cc_ET5_C1_Access_to_external_results_functionality_is_restricted_to_Clinical_Level_users_and_above() 
+//==============================================================================//
+
+//ET5 External Treatment Management Functionality
+
+function cc_ET5_C1_access_to_external_results_functionality_is_restricted_to_clinical_level_users_and_above() 
+
 {
   //add all tests written to cover the above clinical control
   tc_permissions_new_inr_button_make_sure_correct_permission_levels_are_applied_for_coventry_dosing();
@@ -9,4 +14,12 @@ function cc_ET5_C1_Access_to_external_results_functionality_is_restricted_to_Cli
   tc_permissions_new_inr_button_make_sure_correct_permission_levels_are_applied_for_tait_dosing();
   tc_permissions_new_inr_button_make_sure_correct_permission_levels_are_applied_for_oates_dosing_uk_only()
   tc_permissions_new_inr_button_make_sure_correct_permission_levels_are_applied_for_fast_dosing_uk_only();
+}
+
+function cc_ET5_C2_duplicated_messages_identified_and_highlighted_in_the_work_list()
+{
+  //Only done Italy for Instrument
+  //Add in HL7 when doing the UK for completeness of the control
+  
+  
 }
--- Automation/TestComplete/Script/TS_Clinical_Controls.js
+++ Automation/TestComplete/Script/TS_Clinical_Controls.js
@@ -1,17 +1,17 @@
-﻿//--------------------------------------------------------------------------------
+﻿//USEUNIT TC_Clinical_Controls
+
+//--------------------------------------------------------------------------------
 //Suite of tests for all clinical control test
 //--------------------------------------------------------------------------------
-
 //Master Suites
 //==============================================================================//
 //Contains all clinical control tests
 function ts_clinical_control_master()
 {
-  cc_ET5_C1_Access_to_external_results_functionality_is_restricted_to_Clinical_Level_users_and_above();
+  cc_ET5_C1_access_to_external_results_functionality_is_restricted_to_clinical_level_users_and_above();
 }
 
-function cs_ldx2_barcode_scanning() //clinical control parent / risk name
+function cs_ET5_external_treatment_management_functionality() //clinical control parent / risk name
 {
-  //note this control does not match with the parent this is currently just an example, update when possible
-  cc_ET5_C1_Access_to_external_results_functionality_is_restricted_to_Clinical_Level_users_and_above(); //specific clinical control identified for parent risk
+  cc_ET5_C1_access_to_external_results_functionality_is_restricted_to_clinical_level_users_and_above(); //specific clinical control identified for parent risk
 }
