aa60f8ed8095af84a8e57ea9e586b4247065ce49
Dean Lester <dean.lester@lumiradx.com>
2021-03-29 15:17:11 +0100

    removed space

--- Automation/TestComplete/Script/Get_Functions.js
+++ Automation/TestComplete/Script/Get_Functions.js
@@ -44,8 +44,7 @@ function get_patient_details_object_from_demographics()
   patient_details.dob = patient_demographics_tab_demographics_path.Panel(5).Label("Born_DetachedLabel").contentText;
   patient_details.dob_as_dd_mm_yyyy = convert_date_from_dd_mmm_yyyy_to_get_date_as_dd_mm_yyyy(patient_details.dob);
   patient_details.gender = patient_demographics_tab_demographics_path.Panel(7).Label("Gender_DetachedLabel").contentText.substring(0,1); //returns M or F
-  patient_details.fullname = patient_details.lastname + ', ' + patient_details.firstname;
-  
+  patient_details.fullname = patient_details.lastname + ', ' + patient_details.firstname;  
 
   return patient_details;
 }
