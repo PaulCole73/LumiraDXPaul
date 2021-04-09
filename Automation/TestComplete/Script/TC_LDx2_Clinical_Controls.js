d8df3bcf742c58ffd3ecd951d2e6b631d5511198
Dean Lester <dean.lester@lumiradx.com>
2021-04-09 09:06:16 +0100

    Started LDx2 controls

---
+++ Automation/TestComplete/Script/TC_LDx2_Clinical_Controls.js
@@ -0,0 +1,17 @@
+﻿//USEUNIT TC_Treatment
+//USEUNIT TC_Results
+
+//==============================================================================//
+//LDx2 Barcode Scanning / Error in scanning the patient barcode ID within INRstar resulting in an inability to confirm demographics
+//==============================================================================//
+
+function cc_LDx2_C5_patient_identity_is_displayed_on_banner_visible_on_all_patient_specific_screens() 
+{
+
+} 
+//==============================================================================//
+function cc_LDx2_C6_patient_identifiers_displayed_in_inrstar_external_results_list()
+{
+  //tests to cover control go here
+}
+//==============================================================================//
