c713834fc413a1ed1c47a35e62eee548ca0dcc9c
Dean Lester <dean.lester@lumiradx.com>
2021-04-12 11:58:19 +0100

    Added unknown file

--- Automation/TestComplete/Script/TC_LDx2_Clinical_Controls.js
+++ Automation/TestComplete/Script/TC_LDx2_Clinical_Controls.js
@@ -15,11 +15,12 @@ function cc_LDx2_C5_patient_identity_is_displayed_on_banner_visible_on_all_patie
 function cc_LDx2_C6_patient_identifiers_displayed_in_inrstar_external_results_list()
 {
   //Not sure if this control exists for other functionality i.e engage/hl7/rovigo api - If it does then need to add in other patient identifiers
-  tc_patient_column_contains_the_patient_details_provided_by_instrument();
+  tc_patient_column_contains_the_patient_details_for_an_instrument_result();
 }
 //==============================================================================//
 function cc_LDx2_C18_concurrency_error_displayed_when_demographics_amended_before_saving_the_treatment()
 {
+  //This may fail in the UK build as there were changes made for Italy and not in the UK and pushed back up
   tc_concurrency_error_displayed_when_demographics_amended_before_saving_the_treatment();
 }
 //==============================================================================//
