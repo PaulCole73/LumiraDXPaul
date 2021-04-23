0980bacf47096539a75fd98833f3d531a1533e7e
Tom Simons LDxCS <tom.simons@lumiradx.com>
2021-04-23 10:09:42 +0100

    Added functions to files, renamed files

---
+++ Automation/TestComplete/Script/tc_account_management_edit_location_admin_level_user_details.js
@@ -0,0 +1,40 @@
+﻿//USEUNIT Admin_Dash_Get_Functions
+//USEUNIT Admin_Dash_Navigation
+//USEUNIT Misc_Functions
+//USEUNIT TSA_AD_Account_Management
+//USEUNIT TSA_AD_Login
+//--------------------------------------------------------------------------------
+
+function tc_account_management_edit_admin_lead_user()
+{
+  try
+  {
+    var test_title = "Admin Dashboard - Account Management - Edit Location Admin Details";
+    login_admin_dash();
+    
+    var parent_client = "LDxCS-Test-AutoTest1";
+    var username = add_a_new_user(parent_client, "location admin");
+    
+    var new_fullname = get_unique_number();
+    var new_username = get_unique_number();
+    update_a_location_admin(parent_client, username, "edit", new_fullname, new_username);
+    
+    var audit_text = get_audit_entry_admin_dash(1);
+    var result_set = new Array();
+    var result_set_1 = compare_values("Updated a Location Administrators user details", audit_text, test_title);
+    result_set.push(result_set_1);
+  
+    var results = results_checker_are_true(result_set);
+    results_checker(results, test_title);
+    
+    log_off_admin_dash();
+  }
+  catch(e)
+  {
+    Log.Warning("Test \"" + test_title + "\" FAILED Exception Occured = " + e);
+    var suite_name = "tc_account_management_edit_location_admin_level_user_details";
+    var test_name = "tc_account_management_edit_admin_lead_user";
+    handle_failed_tests(suite_name, test_name);
+    restart_admin_dash();
+  }
+}
---
+++ Automation/TestComplete/Script/tc_account_management_remove_an_organisation_level_user.js
@@ -0,0 +1,38 @@
+﻿//USEUNIT Admin_Dash_Get_Functions
+//USEUNIT Admin_Dash_Navigation
+//USEUNIT Misc_Functions
+//USEUNIT TSA_AD_Account_Management
+//USEUNIT TSA_AD_Login
+//--------------------------------------------------------------------------------
+
+function tc_account_management_remove_org_clinical_lead()
+{
+  try
+  {
+    var test_title = "Admin Dashboard - Account Management - Remove Org Clinical Lead User";
+    login_admin_dash();
+    
+    var parent_client = get_parent_client_name();
+    var username = add_a_new_user(parent_client, "org clinical");
+    
+    update_a_user(parent_client, username, "remove");
+    
+    var audit_text = get_audit_entry_admin_dash(1);
+    var result_set = new Array();
+    var result_set_1 = compare_values("Removed clinical lead from organisation account", audit_text, test_title);
+    result_set.push(result_set_1);
+  
+    var results = results_checker_are_true(result_set);
+    results_checker(results, test_title);
+    
+    log_off_admin_dash();
+  }
+  catch(e)
+  {
+    Log.Warning("Test \"" + test_title + "\" FAILED Exception Occured = " + e);
+    var suite_name = "tc_account_management_remove_org_clinical_lead";
+    var test_name = "tc_account_management_remove_org_clinical_lead";
+    handle_failed_tests(suite_name, test_name);
+    restart_admin_dash();
+  }
+}
