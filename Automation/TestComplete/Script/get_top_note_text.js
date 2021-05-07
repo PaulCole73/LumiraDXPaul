4ac4d3cb5768ecea8912915184ec85d03ba9405d
Tom Simons LDxCS <tom.simons@lumiradx.com>
2021-05-07 14:25:50 +0100

    changed name

---
+++ Automation/TestComplete/Script/get_top_note_text.js
@@ -0,0 +1,13 @@
+﻿//USEUNIT Misc_Functions
+//USEUNIT System_Paths
+//USEUNIT INRstar_Navigation
+
+//-----------------------------------------------------------------------------------
+function get_top_note_text()
+{
+  Goto_Patient_Notes();
+  var note_text_path = notes_tab().Table("PatientNotesTable").Cell(1, 0);
+  var text = aqString.Trim(note_text_path.innerText);
+  
+  return text;
+}
