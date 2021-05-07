cd92d397d28ef5ac9321c8ce30d0de64deabdd3f
Dean Lester <dean.lester@lumiradx.com>
2021-05-07 11:01:24 +0100

    Added region comments

--- Automation/TestComplete/Script/TS_Patient.js
+++ Automation/TestComplete/Script/TS_Patient.js
@@ -49,9 +49,14 @@ function ts_int_regression_patient_italy(send_mail)
 //==============================================================================//
 function ts_inrstar_patient_search()
 {
+  //UK Only
   tc_user_can_search_for_a_patient_with_partial_or_full_nhs_number();
   tc_user_can_search_for_a_patient_when_putting_whitespace_anywhere_in_the_search_box_with_nhs_number();
+  
+  //Italy Only
   tc_user_can_search_for_a_patient_with_partial_or_full_fiscal_code();
   tc_user_can_search_for_a_patient_when_putting_whitespace_anywhere_in_the_search_box_with_fiscal();
 }
 //==============================================================================//
+
+
