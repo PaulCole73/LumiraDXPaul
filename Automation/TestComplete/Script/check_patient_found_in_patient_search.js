790a53aa2c4f851d20ef7ef988e1dc2b585c8d8f
Tom Simons LDxCS <tom.simons@lumiradx.com>
2021-05-10 09:04:57 +0100

    changes

--- Automation/TestComplete/Script/check_patient_found_in_patient_search.js
+++ Automation/TestComplete/Script/check_patient_found_in_patient_search.js
@@ -9,6 +9,8 @@ function check_patient_found_in_patient_search_table(search_criteria, expected_p
   
   if(results_table.Cell(1, 0).contentText != get_string_translation("No patient found"))
   {
+    
+    check_patient_exists_in_table_within_column()
 	  for(var i = 0; i < results_table.rowCount; i++)
 	  {
 		  if(results_table.Cell(i, 0).contentText == expected_patient_fullname)
